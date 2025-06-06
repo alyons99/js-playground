ea39e7bfadb52a4cdd12b204e7d2332572d2e902
Andrew Lyons <andrew.r.lyons99@gmail.com>
2025-06-06 14:04:53 -0400

    finised card learning

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
-import Food from "./Food.jsx"
+import Card from "./Card.jsx"
 
       {/*This is called a fragment. Because the return function can only return one thing,
         we make all of our compontents children of the fragment */}
@@ -8,10 +8,10 @@ import Food from "./Food.jsx"
 function App() {
     return(
       <>
-        <Header/>
-        <Food/>
-        <Food/>
-        <Footer/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
       </>
 
     );
---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+function Card(){
+    
+    return(
+        <div className="card">
+            <img className = "card_image" src="https://placehold.co/200" alt="profile picture"></img>
+            <h2 className="card_title">Drew</h2>
+            <p className="card_description">Learning React for a job!</p>
+        </div>
+    );
+}
+
+export default Card
